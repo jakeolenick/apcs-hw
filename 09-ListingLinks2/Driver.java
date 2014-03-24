5b5bbbb8bb72ac4b2c20f89439476d093463eed1
Jake Olenick <jakeolenick@gmail.com>
2014-03-24 00:13:45 -0400

    moved modified linked lists to new 09 folder

---
+++ 09-ListingLinks2/Driver.java
@@ -0,0 +1,16 @@
+public class Driver{
+
+    public static void main(String[] args){
+	MyLinkedList m = new MyLinkedList();
+       
+	m.add("A");
+	m.add("B");
+	m.add(3);
+	m.add(2,10);
+	System.out.println(m);
+	System.out.println(m.remove(2));
+	System.out.println(m.length());
+	
+	
+    }
+}
